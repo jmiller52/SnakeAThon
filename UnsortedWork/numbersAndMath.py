db323380bdb16e636144260062f4a8aa5e1d47ee
Joshua Miller <jm665965@c02yk2nqjgh6.dhcp.broadcom.net>
2019-08-28 10:02:48 -0600

    Adding in Numbers and Math

---
+++ UnsortedWork/numbersAndMath.py
@@ -0,0 +1,32 @@
+# Integer is a whole number. Positive and Negative
+# Floating point has a decimal place. Positive and Negative
+
+# Simple math
+12 + 1
+# eduals 13 of course
+12 + 1.1
+# changes int to float because of the '.1'
+
+type(9)  # results in type '<class 'int'>'
+# Tells us the type of the value in the parentheses
+type('string')  # results in type '<class 'str'>'
+type('2')  # also restults in type '<class 'str'>'
+
+# remember the Order of Operations 'PEMDAS'
+# Parentheses
+# Exponents
+# Multiplication
+# Division
+# Addition
+# Subtraction
+
+'''OPERATORS
+Symbol  Name
++       Addition
+-       Subtraction
+*       Multiplication
+/       Division
+**      Exponentiation
+%       Modulo
+//      Integer Division
+'''
