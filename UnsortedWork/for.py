6a142cab3457f3066208b0b83ca65b80f112a40f
Joshua Miller <jm665965@c02yk2nqjgh6.dhcp.broadcom.net>
2019-09-24 09:56:07 -0600

    quick update

---
+++ UnsortedWork/for.py
@@ -0,0 +1,11 @@
+# for x in range(1,10):
+#     print(x)
+
+# for char in "coffee":
+#     print(char)
+
+for num in range(10):
+    print(num)
+
+for num2 in range(10,20,2):
+    print(num2)
---
+++ UnsortedWork/notes.py
@@ -0,0 +1,36 @@
+# yamaha
+#     printPhoto
+#     printPrice
+#     calcLoc
+# wurlitzer
+#     printPhoto
+#     printPrice
+#     calcLoc
+# and so on...
+
+
+# Change to 
+# for every piano
+#     printPhoto
+#     printPrice
+#     calcLoc
+
+# a "for" loop refers to every character in a string, or item in a list, or value in a range, etc...
+
+#  for "item" in "iterable_object"
+    # An iterable object is some kind of collection of items, for instance: a list of numbers, a string of charactersm a range, etc...
+    # item is a new variable that can be called whatever you want (within the limirations of python of course)
+    # item references the current position of our iterator within the iterable. It will iterate over (run through) every item of the collection and then fo away when it has visited all items
+
+# Ranges
+# a way to represent a sequence of numbers commonly used in loops
+
+# range(1,10) = 1,2,3,4,5,6,7,8,9
+# started at 1 and ended when it say the last number but does not include it
+
+# range(7) = 0,1,2,3,4,5,6
+# started at 0 and ended at 6 because it does not include the last number
+
+# range(1,10,2) will give you ods from 1 to 10
+
+# range(7,0,-1) will give integers from 7 to 1 because the "-1" goes in reverse
