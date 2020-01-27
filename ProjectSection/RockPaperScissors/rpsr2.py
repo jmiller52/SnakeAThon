5b8fc891c3386b42104432c2302ea7897dae4e22
Josh Miller <jmiller52.ca@gmail.com>
2020-01-27 08:25:28 -0700

    round 2 for rock paper scissors

---
+++ ProjectSection/RockPaperScissors/rpsr2.py
@@ -0,0 +1,25 @@
+print("...rock...")
+print("...paper...")
+print("...scissors...")
+
+player1 = input("(enter player 1's choice): ")
+player2 = input("(enter player 2's choice): ")
+print("SHOOT!")
+
+if player1 == player2:
+    print("It's a draw")
+elif player1 == "rock":
+    if player2 == "paper":
+        print("player2 wins")
+    else:
+        print("player1 wins")
+elif player1 == "paper":
+    if player2 == "rock":
+        print("player1 wins")
+    else:
+        print("player2 wins")
+elif player1 == "scissors":
+    if player2 == "rock":
+        print("player2 wins")
+    else:
+        print("player1 wins")
